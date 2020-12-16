c716b339bed6cca0d3ef2f12fea744deae603bc5
Paweł Waśkiewicz <pawel.waskiewicz@gmail.com>
2020-12-16 16:55:54 +0100

    Add find the correct article and class 'active' to the correct article

--- js/script.js
+++ js/script.js
@@ -28,8 +28,10 @@ for(let activeLink of activeLinks){
 const articleSelector = clickedElement.getAttribute('href');
 console.log(articleSelector)
   /* find the correct article using the selector (value of 'href' attribute) */
-
+const targetArticle = document.querySelector(articleSelector);
+console.log(targetArticle)
   /* add class 'active' to the correct article */
+  targetArticle.classList.add('active');
   }
 
   const links = document.querySelectorAll('.titles a');
